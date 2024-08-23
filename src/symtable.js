4f01c52f862f403d509d32f13b675081672cd08e
Ondřej Hruboš <hruboson@gmail.com>
2024-08-23 12:32:23 +0200

    feat(symtable): beginning

---
+++ src/symtable.js
@@ -0,0 +1,65 @@
+class ScopeInfo {
+	name;
+	type;
+	level;
+
+	constructor(name, type, level){
+		this.name = name;
+		this.type = type;
+		this.level = level;
+	}
+}
+
+class Symbol {
+	name;
+	type;
+	scope; // typeof Scope
+	initialized;
+	address;
+
+	constructor(name, type){
+		this.name = name;
+		this.type = type;
+		this.scope = scope;
+	}
+}
+
+/**
+ * Scoped symbol table
+ * @class Symtable
+ * @param {Symtable} [parent=null] Parent scoped symbol table
+ */
+class Symtable {
+	/**
+	 * Parent symtable
+	 * @type {Symtable}
+  	 */
+	parent;
+	
+	/**
+	 * Data about current scope
+	 * @type {ScopeInfo}
+	 */
+	scopeInfo;
+
+	/**
+	 * Symbol table
+	 * @type {Map<String, Symbol>}
+	 */
+	symbols;
+
+	constructor(name, type, parent=null){
+		this.scope = parent === null ? new ScopeInfo(name, type, 1) : new ScopeInfo(name, type, parent.scopeInfo.level + 1);
+		this.parentSymtable = parent;
+	}
+	
+	insert(symbol){
+
+	}
+
+	lookup(name){
+
+	}
+}
+
+
